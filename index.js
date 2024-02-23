08ed9d605bda77cd7e6efc971167119d99545b8c
Heba ElSayed Abd ElKareem <56650005+he15ba11@users.noreply.github.com>
2024-02-23 16:24:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const express=require("express")
-
+require("dotenv").config();
+const express = require('express');
+require('./db')
 const app=express();
 app.use(express.json())
 
 
-/////////             Aml
+/////////             Heba
+
+const  userRoutes = require('./routes/userRoutes');
+const profileRoutes = require('./routes/profileRoutes')
+
+app.use(express.static("public"))
+
+app.use('/api/v1/user', userRoutes);
+
+app.use("/api/v1/profile", profileRoutes);
+
+app.listen(3000, () => {
+  console.log(`Listening on port 3000 ....`);
+});
+
+
+
+
+
+////////////////// amal 
 
 
 
@@ -12,17 +32,9 @@ app.use(express.json())
 
 
 
-////////////////// radwa 
 
 
-
-
-
-
-
-
-
-////////////////   heba
+////////////////   radwa
 
 
 
