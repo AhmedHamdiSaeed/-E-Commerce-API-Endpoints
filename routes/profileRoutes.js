076ca345fdb7afd35f5809eb17221e7ab4ad15af
Heba ElSayed Abd ElKareem <56650005+he15ba11@users.noreply.github.com>
2024-02-23 16:21:10 +0200

    Add files via upload

---
+++ routes/profileRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const profileController = require("../controllers/profileController");
+const {auth} = require("../middleware/auth");
+
+router.get("/",auth, profileController.getCurrentUser);
+router.patch("/:id",auth, profileController.updateProfile);
+
+module.exports = router;
