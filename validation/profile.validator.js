035b915f63fd9094e7d434459321826827b27bc9
Ahmed Hamdi <ahmedhamdisaeed@gmail.com>
2024-02-25 14:50:21 +0200

    delete role from validation

--- validation/profile.validator.js
+++ validation/profile.validator.js
@@ -4,9 +4,7 @@ const userSchema = Joi.object({
   fname: Joi.string().required().min(3).max(10),
   lname: Joi.string().required().min(3).max(10),
   email: Joi.string().required(),
-  password: Joi.string().required().min(6),
-  role : Joi.string().required()
-});
+  password: Joi.string().required().min(6)});
 
 module.exports = userSchema;
 
