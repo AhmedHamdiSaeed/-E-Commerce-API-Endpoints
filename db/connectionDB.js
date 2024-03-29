3db5567244d1762df9b147c64eff1707cc77a092
Emad Ramdan <emadramdan999@gmail.com>
2024-03-29 04:51:22 -0700

    uploade products image from front to backend

--- db/connectionDB.js
+++ db/connectionDB.js
@@ -2,9 +2,10 @@
 const mongoose=require("mongoose")
 require("dotenv").config()
 
-// const path = "mongodb+srv://user1:1111@e-commercedb.ibnxepr.mongodb.net/e-commercedb"
-// const path = "mongodb://user1:1111@e-commercedb.ibnxepr.mongodb.net/e-commercedb"
-const path = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1"
+const path = "mongodb+srv://user1:1111@e-commercedb.ibnxepr.mongodb.net/e-commercedb"
+// const path = "mongodb://user1:1111@e-commercedb.ibnxepr.mongodb.net/e-commercedb" ;
+// const path = "mongodb://user1:1111@e-commercedb.ibnxepr.mongodb.net:27017/"
+// const path = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1"
 
 mongoose.connect(path).then(()=>console.log("connect ")).catch(()=>console.log("no connect"))
 
