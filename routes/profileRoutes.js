4496c6cbed66197141da97d11109e94abd358575
Ahmed Hamdi <ahmedhamdisaeed@gmail.com>
2024-04-02 21:48:46 +0200

    edite userProfile

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -5,6 +5,4 @@ const {auth} = require("../middleware/auth");
 
 router.get("/",auth, profileController.getCurrentUser);
 router.patch("/:id",auth, profileController.updateProfile);
-
-
 module.exports = router;
