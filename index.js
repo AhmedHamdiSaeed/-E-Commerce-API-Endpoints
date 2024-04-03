5fecc9c87809fb3f0b0562f30158c109081957df
Emad Ramdan <emadramdan999@gmail.com>
2024-04-03 01:57:44 -0700

    modified admin validation in creat category

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(compression())
-app.use('/api/v1/images' , express.static(path.join(__dirname , '/uploads'))) ;
+app.use('/api/v1/uploads' , express.static(path.join(__dirname , '/uploads'))) ;
 
 
 
